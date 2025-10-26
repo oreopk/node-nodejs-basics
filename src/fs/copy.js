ccd972943262580bab0e652e276987552ddd2b43
oreopk <sart6zaV@yandex.ru>
2025-10-26 20:28:17 +0300

    feat: copy js done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+
 const copy = async () => {
-  // Write your code here
+    const __dirname = import.meta.dirname;
+    const sourseFiles = path.join(__dirname, "files");
+    const sourseFiles_copy = path.join(__dirname, "files_copy");
+    if (!fs.existsSync(sourseFiles) || fs.existsSync(sourseFiles_copy)) {
+        throw new Error('FS operation failed');
+    }
+    fs.mkdirSync(sourseFiles_copy);
+    fs.readdirSync(sourseFiles).map(file => {
+        const sourcePath = path.join(sourseFiles, file);
+        const targetPath = path.join(sourseFiles_copy, file);
+        fs.copyFileSync(sourcePath, targetPath);
+    })
 };
 
 await copy();
