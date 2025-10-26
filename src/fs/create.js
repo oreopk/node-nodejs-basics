564803e31011a6fcc82e0a7e5b7f40d2e59a391e
oreopk <sart6zaV@yandex.ru>
2025-10-26 19:25:04 +0300

    feat: create js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
 const create = async () => {
-  // Write your code here
+  // const filePath = path.join('src/fs/files', 'fresh.txt');
+  const filePath = './src/fs/files/fresh.txt';
+  if (fs.existsSync(filePath)) {
+      throw new Error('FS operation failed');
+  }
+  fs.writeFileSync(filePath, 'I am fresh and young');
 };
 
 await create();
