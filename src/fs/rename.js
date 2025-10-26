5adc2a1f3bebca88c76367bd9c7bb526e1a554fa
oreopk <sart6zaV@yandex.ru>
2025-10-27 02:43:11 +0300

    feat: rename and read js done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
 const rename = async () => {
-  // Write your code here
+    const __dirname = import.meta.dirname;
+    const oldPath = path.join(__dirname, "files", 'wrongFilename.txt');
+    const newPath = path.join(__dirname, "files", 'properFilename.md');
+    if (!fs.existsSync(oldPath) || fs.existsSync(newPath)) {
+        throw new Error('FS operation failed');
+    }
+     fs.renameSync(oldPath, newPath);
 };
 
 await rename();
